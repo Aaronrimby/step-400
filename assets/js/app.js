e04d1f03f0acd39dd627a6cfb52b4c67f7bffba9
aaronrimby <aaronrimby@gmail.com>
2018-03-18 15:35:28 -0400

    added progress bar, displayPB function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,14 @@
 // JavaScript Document
- 
-
 'use strict';
 
 var appData = {
     title: 'inside out - js',
-    tagLine: 'continuously falling into the light...'
+    tagLine: 'continuously falling forward into the light...'
 };
 
 window.addEventListener('load', initializeApplication);
 
-function initializeApplication(){
+function initializeApplication() {
     
     document.title = appData.title;
     
@@ -32,22 +30,40 @@ function initializeApplication(){
     
     var elHeaderTagLine = document.createElement('h2');
     elHeaderTagLine.textContent = appData.tagLine;
-    elHeader.appendChild(elHeaderTagLine);
-     elHeaderTagLine.className = 'animated bounceInLeft';
     elHeaderTagLine.style.fontSize = '.4em';
+    elHeader.appendChild(elHeaderTagLine);
+    elHeaderTagLine.className = 'animated bounceInLeft';    
     
     var elMain = document.createElement('main');
     elMain.innerHTML = '<p>Hello World!</p>';
     elWrapper.appendChild(elMain);
-     elMain.className = 'animated zoomIn';
+    elMain.className = 'animated zoomIn';
     
     var elFooter = document.createElement('footer');
-    elFooter.innerHTML = '<h4>get excited about learning Javascript...</h4>';
+    elFooter.innerHTML = '<h4>get excited about learning JavaScript...</h4>';
     elWrapper.appendChild(elFooter);
     elFooter.className = 'animated bounceInRight';
     
+    elMain.innerHTML += '<div style="width: 40%; margin: auto; padding:10px; margin-bottom: 20px;"><div class="progress" style="height: 20px;"><div id="loaderProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
+
+     displayPB();
 }
 
+
+var timerCount = 0;
+function displayPB() {
+    if (timerCount <= 100) {
+        var x = (timerCount < 25) ? '' : (timerCount < 45) ? timerCount + '%' : (timerCount < 65) ? 'Loading ' + timerCount + '%' : 'Loading Application ' + timerCount + '%';
+        document.getElementById("loaderProgressBar").innerHTML = x;
+        document.getElementById('loaderProgressBar').setAttribute('aria-valuenow', timerCount);
+        document.getElementById('loaderProgressBar').style.width = timerCount + '%    ';
+        timerCount++;
+        setTimeout(displayPB, 50);
+    } else {
+        timerCount = 0;
+        return false;
+    }
+}
 
 
 
